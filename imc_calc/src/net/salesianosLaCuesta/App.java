fdf7f1546fe0b5a858fc194fc43ba033581d5f3a
ElMafiosodeJava <152169515+ElMafiosodeJava@users.noreply.github.com>
2025-10-20 19:02:33 +0100

    refactor(App): Eliminación de imports innecesarios

--- imc_calc/src/net/salesianosLaCuesta/App.java
+++ imc_calc/src/net/salesianosLaCuesta/App.java
@@ -1,12 +1,7 @@
 package net.salesianosLaCuesta;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+
 import java.util.ArrayList;
 
 import net.salesianosLaCuesta.utils.FileHelper;
@@ -15,6 +10,7 @@ import net.salesianosLaCuesta.utils.ProcessLauncher;
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
     
+        
     FileHelper file = new FileHelper();
 
     ArrayList <String> phrases = file.leerDocumento();
